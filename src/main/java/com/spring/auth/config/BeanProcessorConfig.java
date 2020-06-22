bba82f97666dde301b5936cfb903c1747a8a2fee
Diego Tobalina <diegotobalina97@gmail.com>
2020-06-22 21:09:51 +0200

    fix tests

---
+++ src/main/java/com/spring/auth/config/BeanProcessorConfig.java
@@ -0,0 +1,19 @@
+package com.spring.auth.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("!dev")
+public class BeanProcessorConfig implements BeanFactoryPostProcessor {
+  @Override
+  public void postProcessBeanFactory(final ConfigurableListableBeanFactory beanFactory)
+      throws BeansException {
+    for (final String beanName : beanFactory.getBeanDefinitionNames()) {
+      beanFactory.getBeanDefinition(beanName).setLazyInit(true);
+    }
+  }
+}
