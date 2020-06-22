7c98ded14c4591bfc248b718bec151a3e032f34f
Diego Tobalina <diegotobalina97@gmail.com>
2020-06-22 23:26:12 +0200

    CustomPermissionEvaluator refactor

--- src/main/java/com/spring/auth/components/CustomPermissionEvaluator.java
+++ src/main/java/com/spring/auth/components/CustomPermissionEvaluator.java
@@ -1,16 +1,13 @@
 package com.spring.auth.components;
 
 import com.spring.auth.scope.domain.Scope;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 /**
  * Evaluate scope permissions
@@ -18,30 +15,25 @@ import java.util.stream.Collectors;
  * @author diegotobalina
  */
 @Component
-@Slf4j
 public class CustomPermissionEvaluator implements PermissionEvaluator {
+
   @Override
   public boolean hasPermission(
-      final Authentication authentication, final Object accessType, final Object permission) {
+      Authentication authentication, Object accessType, Object permission) {
+    String requiredPermission = String.valueOf(permission);
+    List<Scope> validScopes = getValidScopes(authentication, requiredPermission);
+    return (validScopes.isEmpty()) ? false : true;
+  }
 
-    // input validations
-    final String requiredPermission = String.valueOf(permission);
-    if (Objects.isNull(authentication)) return false;
-    if (Objects.isNull(authentication.getCredentials())) return false;
-    if (ObjectUtils.isEmpty(authentication.getCredentials())) return false;
-    if (!(authentication.getCredentials() instanceof List<?>)) return false;
+  private List<Scope> getValidScopes(Authentication authentication, String requiredPermission) {
+    List<Scope> scopes = (List<Scope>) authentication.getCredentials();
+    List<Scope> validScopes = new ArrayList<>();
+    for (Scope scope : scopes) if (isValidScope(requiredPermission, scope)) validScopes.add(scope);
+    return validScopes;
+  }
 
-    // check if the user have the needed permissions ( scope )
-    final List<Scope> scopes = (List<Scope>) authentication.getCredentials();
-    final List<Scope> validScopes =
-        scopes.stream()
-            .filter(scope -> requiredPermission.startsWith(scope.getValue()))
-            .collect(Collectors.toList());
-
-    if (validScopes.isEmpty()) { // user has not the required permissions
-      return false;
-    }
-    return true;
+  private boolean isValidScope(String requiredPermission, Scope scope) {
+    return requiredPermission.startsWith(scope.getValue());
   }
 
   @Override
