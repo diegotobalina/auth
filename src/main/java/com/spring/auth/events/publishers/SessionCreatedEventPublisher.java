64ffe2f8c1dda052fc79010a3a981d92484e1d52
Diego Tobalina <diegotobalina97@gmail.com>
2020-06-28 03:20:07 +0200

    fix implements

--- src/main/java/com/spring/auth/events/publishers/SessionCreatedEventPublisher.java
+++ src/main/java/com/spring/auth/events/publishers/SessionCreatedEventPublisher.java
@@ -2,14 +2,14 @@ package com.spring.auth.events.publishers;
 
 import com.spring.auth.anotations.components.CustomEventPublisher;
 import com.spring.auth.events.domain.SessionCreatedEvent;
-import com.spring.auth.events.ports.PublishSessionUsedEventPort;
+import com.spring.auth.events.ports.PublishSessionCreatedEventPort;
 import com.spring.auth.session.domain.Session;
 import org.springframework.context.ApplicationEventPublisher;
 
 /** @author diegotobalina created on 19/06/2020 */
 @CustomEventPublisher
 public class SessionCreatedEventPublisher extends EventPublisher
-    implements PublishSessionUsedEventPort {
+    implements PublishSessionCreatedEventPort {
 
   public SessionCreatedEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
     super(applicationEventPublisher);
